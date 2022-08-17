9adcc764e2f900e7d7b4796c5751364e4a48354c
yunji <60842443+yunjigo92@users.noreply.github.com>
2022-08-17 10:11:21 +0900

    Config #1 - spring configuration
    기초설정 추가

---
+++ src/main/java/com/pastcampus/springpractice/config/Config.java
@@ -0,0 +1,15 @@
+package com.pastcampus.springpractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * description
+ * <p>
+ * author         : yunji
+ * date           : 22. 8. 16.
+ */
+@ComponentScan("com.pastcampus.springpractice")
+@Configuration
+public class Config {
+}
